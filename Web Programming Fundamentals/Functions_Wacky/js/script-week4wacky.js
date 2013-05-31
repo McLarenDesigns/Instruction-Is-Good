b90b02ec536262516125d9251ec1865cc77e1c8f
McLarenDesigns <Shauntime@yahoo.com>
2013-05-31 18:27:56 -0400

    Cleaned up the comments and made sure they were correct

--- Web Programming Fundamentals/Functions_Wacky/js/script-week4wacky.js
+++ Web Programming Fundamentals/Functions_Wacky/js/script-week4wacky.js
@@ -1,11 +1,11 @@
 //Shaun Mcaren 5,31,2013 Assignment: Functions Wacky
 
 
-var weightClass = prompt("How many pounds can you lift?", "");
+var weightClass = prompt("How many pounds can you bench?", "");
 //Tells JavaScript to bring up a prompt window that asks, "What is your last name?"
 
 if (weightClass >= 200 )
-//This tells JavaScript that "if" the name is Silvay, then it should perform the set of instructions below.
+//This tells JavaScript that "if" variable "weightClass" is greater than or equal to 200, then it should perform the set of instructions below.
 {
 		document.write("<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />Somebody likes their wheaties!! <br /> <br />");
 		//Tells JavaScript to write this on the page 
@@ -25,7 +25,7 @@ if (answer == "Yes")
 	document.write("Smack your six pack!!! <br /> <br /> <br />");
 
 }
-//I told JavaScript that the variable "answer" is to be the same as the input recieved from the user. if that input is equal to "yes" than it should write "Preparing your beginners pack" followed by three line breaks.
+//I told JavaScript that the variable "answer" is to be the same as the input recieved from the user. if that input is equal to "yes" than it should write "Smack your six pack!!!" followed by three line breaks.
 var name2 = prompt("What is your name?", "");
 //I tell Javascript that the variable name2 will come from user input.
 document.write(name2 +", " + "when your pack is ready, you may venture outside to explore the village. Interact with any person you choose, but beware of shady characters. They will try to steal your gold!!");
