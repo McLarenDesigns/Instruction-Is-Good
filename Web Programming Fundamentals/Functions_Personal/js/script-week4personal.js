f9afdf3ca7cb3792de7afd53e099be31ee0d4fd3
McLarenDesigns <Shauntime@yahoo.com>
2013-05-31 16:01:08 -0400

    Moved the script from "learn functions example" into the .js file
    Everything is working great. I just need to add some comments now.

---
+++ Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
@@ -0,0 +1,17 @@
+
+
+
+name = prompt("What is your last name?", "");
+
+
+if (name == "Silvay")
+{
+		document.write("Wow!! Could this be the wise and great one from the honorable Silvay family?... <br />");
+		
+		function beginnersPack (strParam) {
+    alert(strParam);
+}
+ 
+setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 5000);
+}
+
