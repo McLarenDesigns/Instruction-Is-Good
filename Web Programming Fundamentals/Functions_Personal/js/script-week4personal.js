40bb86d5fbcd8fc5b7612647b4117eb700f6cdaf
McLarenDesigns <Shauntime@yahoo.com>
2013-05-31 16:52:45 -0400

    Remembered there must be two prompts for information
    setting up second prompt now

--- Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
+++ Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
@@ -2,12 +2,13 @@
 
 
 name = prompt("What is your last name?", "");
-
+//Tells JavaScript to bring up a prompt window that asks, "What is your last name?"
 
 if (name == "Silvay")
+//This tells JavaScript that "if" the name is Silvay, then it should perform the set of instructions below.
 {
 		document.write("Wow!! Could this be the wise and great one from the honorable Silvay family?... <br />");
-		
+		//Tells JavaScript to wrrite this on the page 
 		function beginnersPack (strParam) {
     alert(strParam);
 }
@@ -15,3 +16,17 @@ if (name == "Silvay")
 setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 5000);
 }
 
+name2 = prompt("What is your first name?", "")
+
+if (name == "Silvay")
+//This tells JavaScript that "if" the name is Silvay, then it should perform the set of instructions below.
+{
+		document.write("Wow!! Could this be the wise and great one from the honorable Silvay family?... <br />");
+		//Tells JavaScript to wrrite this on the page 
+		function beginnersPack (strParam) {
+    alert(strParam);
+}
+ 
+setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 5000);
+}
+document.write("Hello " + name2 + " Welcome. <br />");
