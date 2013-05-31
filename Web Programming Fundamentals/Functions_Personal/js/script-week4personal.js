26e7f9990d9fef74368a607878ccd78463fa5e50
McLarenDesigns <Shauntime@yahoo.com>
2013-05-31 18:01:01 -0400

    Added comments and fixed typos
    Everything is looking good, I just need to add my name, date, and
    assignment to the html portion of the project

--- Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
+++ Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
@@ -7,23 +7,27 @@ var name = prompt("What is your last name?", "");
 if (name == "Silvay")
 //This tells JavaScript that "if" the name is Silvay, then it should perform the set of instructions below.
 {
-		document.write("Wow!! Could this be the wise and great one from the honorable Silvay family?... <br />");
+		document.write("<br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />Wow!! Could this be the wise and great one from the honorable Silvay family?... <br /> <br />");
 		//Tells JavaScript to write this on the page 
 		function beginnersPack (strParam) 
 		{
     alert(strParam);
 }
  
-setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 20000);
+setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 15000);
 }
+//I told JavaScript what my function was and what the parameters where and then when to execute my function.
+var answer = prompt("Is it you?", "Yes");
 
-var answer = prompt("Would you like to continue?", "Yes");
-
-If (answer == Yes)
+if (answer == "Yes")
 
 {
-	document.write("Preparing your beginners pack")
+	document.write("Preparing your beginners pack <br /> <br /> <br />");
 
 }
+//I told JavaScript that the variable "answer" is to be the same as the input recieved from the user. if that input is equal to "yes" than it should write "Preparing your beginners pack" followed by three line breaks.
+var name2 = prompt("What is your first name?", "");
+//I tell Javascript that the variable name2 will come from user input.
+document.write(name2 +" " + "When your pack is ready, you may venture outside to explore the village. Interact with any person you choose, but beware of shady characters. They will try to steal your gold!!");
+//Then I tell JavaScript that "name2" should be used in conjunction with what i ask to be written on the page.
 
-var name2
