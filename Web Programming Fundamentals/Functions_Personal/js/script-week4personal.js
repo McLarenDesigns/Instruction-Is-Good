45b266ea532c5fcf2ae11aa26e6ce02e5db4e40f
McLarenDesigns <Shauntime@yahoo.com>
2013-05-31 17:30:32 -0400

    going along with the theme, making things operate more smoothly
    I am trying to give the feeling that the user is being accomodated in
    real time.

--- Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
+++ Web Programming Fundamentals/Functions_Personal/js/script-week4personal.js
@@ -1,7 +1,7 @@
 
 
 
-name = prompt("What is your last name?", "");
+var name = prompt("What is your last name?", "");
 //Tells JavaScript to bring up a prompt window that asks, "What is your last name?"
 
 if (name == "Silvay")
@@ -9,24 +9,21 @@ if (name == "Silvay")
 {
 		document.write("Wow!! Could this be the wise and great one from the honorable Silvay family?... <br />");
 		//Tells JavaScript to write this on the page 
-		function beginnersPack (strParam) {
+		function beginnersPack (strParam) 
+		{
     alert(strParam);
 }
  
-setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 5000);
+setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 20000);
 }
 
-name2 = prompt("What is your first name?", "")
+var answer = prompt("Would you like to continue?", "Yes");
 
-if (name == "Silvay")
-//This tells JavaScript that "if" the name is Silvay, then it should perform the set of instructions below.
+If (answer == Yes)
+
 {
-		document.write("Wow!! Could this be the wise and great one from the honorable Silvay family?... <br />");
-		//Tells JavaScript to wrrite this on the page 
-		function beginnersPack (strParam) {
-    alert(strParam);
+	document.write("Preparing your beginners pack")
+
 }
- 
-setTimeout("beginnersPack('You have received 20 Scrolls, 30 Arrows, Beginners armor, and a letter from Lord Yoshuba.')", 5000);
-}
-document.write("Hello " + name2 + " Welcome. <br />");
+
+var name2
